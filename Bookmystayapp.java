fd60afbd8641fc1be4639cf6c12ab6e9d56bce12
CharanVasireddy <charanchinnu0112@gmail.com>
2026-03-24 09:53:45 +0530

    UC2 added

---
+++ Bookmystayapp.java
@@ -0,0 +1,52 @@
+
+    abstract class Room {
+    String type;
+    int beds;
+    double price;
+
+    public Room(String type, int beds, double price) {
+        this.type = type;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public void display() {
+        System.out.println(type + " | Beds: " + beds + " | Price: " + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 2000);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 3500);
+    }
+}
+
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 6000);
+    }
+}
+
+public class Bookmystayapp {
+
+    public static void main(String[] args) {
+
+        Room r1 = new SingleRoom();
+        Room r2 = new DoubleRoom();
+        Room r3 = new SuiteRoom();
+
+        System.out.println("Room Details:");
+        r1.display();
+        r2.display();
+        r3.display();
+    }
+}
+
+ 
+
